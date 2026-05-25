4e2820ce2ddde0fd5a8f2ddcb6b9b012e96abf62
diegoignaciocejas50-oss <diegoignaciocejas50@gmail.com>
2026-05-25 03:57:04 -0300

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  setPersistence,
+  browserLocalPersistence,
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDbnn4m8KDBYun1OxuPocXLDTIuK29xSns",
+  authDomain: "info-transporte-urbano-sf.firebaseapp.com",
+  projectId: "info-transporte-urbano-sf",
+  appId: "1:68233107354:web:42f52795bdf7dcae26cc23",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+
+setPersistence(auth, browserLocalPersistence);
+
+export const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
