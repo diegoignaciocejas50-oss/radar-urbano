2bb575389a160acca7129e23e2f638a47ec08f5f
diegoignaciocejas50-oss <diegoignaciocejas50@gmail.com>
2026-05-31 05:36:20 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import {
   getAuth,
   GoogleAuthProvider,
@@ -14,7 +15,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
+export const db = getFirestore(app);
 export const auth = getAuth(app);
 
 setPersistence(auth, browserLocalPersistence);
